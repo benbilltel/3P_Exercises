36b8f43bd4ccffe57ccc7932af734a38a3a65856
Ben <benbilldev@gmail.com>
2024-03-19 09:30:06 +0700

    QuestionBank-Html/js/css-delete-Delete all project to restart again

--- Question_Bank_Week_01/app.js
+++ Question_Bank_Week_01/app.js
@@ -5,12 +5,14 @@ const PORT = process.env.PORT || 4000;
 const path = require("path");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("question-bank");
 });
 app.listen(PORT, () => {
   console.log(`Server is listening on port http://localhost:${PORT}`);
