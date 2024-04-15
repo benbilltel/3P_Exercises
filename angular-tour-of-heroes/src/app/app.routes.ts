ecb9a4c15cbb33399b027b70ff489bbeef5c8899
benbilltel <97432009+benbilltel@users.noreply.github.com>
2024-04-15 08:53:01 +0700

    Delete angular-tour-of-heroes/src/app directory

--- angular-tour-of-heroes/src/app/app.routes.ts
+++
@@ -1,4 +0,0 @@
-import { Routes } from '@angular/router';
-import { HeroesComponent } from './heroes/heroes.component';
-
-export const routes: Routes = [ { path: 'heroes', component: HeroesComponent }];
